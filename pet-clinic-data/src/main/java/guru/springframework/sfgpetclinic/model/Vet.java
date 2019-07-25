9d1a356b7866f9f329c856c040a0125f9ea61858
Georgi Dimitrov <dgeorgi@vmware.com>
2019-07-25 23:08:56 +0300

    Added Project Lombok to project. closes #51

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -9,6 +13,9 @@ import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -17,12 +24,4 @@ public class Vet extends Person {
    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
    private Set<Speciality> specialities = new HashSet<>();
-
-   public Set<Speciality> getSpecialities() {
-      return specialities;
-   }
-
-   public void setSpecialities(Set<Speciality> specialities) {
-      this.specialities = specialities;
-   }
 }
